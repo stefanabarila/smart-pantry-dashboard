4a16e242d05edc3b061532834edbae8f637f8404
Stefana Barila <s.barila@student.tudelft.nl>
2026-08-19 16:33:34 +0200

    added InventoryItem class

---
+++ backend/src/main/java/entity/InventoryItem.java
@@ -0,0 +1,37 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class InventoryItem {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Setter
+    @Getter
+    @Column
+    private String name;
+
+    @Setter
+    @Getter
+    @Column
+    private int quantity;
+
+    @Setter
+    @Getter
+    @Column
+    private int minThreshold;
+
+    public InventoryItem() {
+    }
+
+    public InventoryItem(String name, int quantity, int minThreshold) {
+        this.name = name;
+        this.quantity = quantity;
+        this.minThreshold = minThreshold;
+    }
+
+}
